83d937ee5e2d6e5a5d92692dbd525c987d8767d3
Saša Barišić <cartman300@net.hr>
2014-09-02 16:40:07 +0200

    Some includes

--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -124,6 +124,12 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 	#define Q_EXPORT
 #endif
 
+#if CLI
+	#using <mscorlib.dll>
+	#using <system.dll>
+	using namespace System;
+#endif
+
 /**********************************************************************
   VM Considerations
 
